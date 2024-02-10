e94c12b24eb569935b53a53e17fe2cfb4b8eaace
Geoff-Bosenbark <158342401+Geoff-Bosenbark@users.noreply.github.com>
2024-02-09 20:48:38 -0600

    Update Move.java
    Updating with concise description of functionality at the top of the source code.

--- Connect-Four-Game/src/application/Move.java
+++ Connect-Four-Game/src/application/Move.java
@@ -1,3 +1,10 @@
+/**
+ * Represents a move in the game by encapsulating the row and column indices into a single object.
+ * This class is utilized primarily within the SmartAI class to facilitate more strategic AI decisions by
+ * allowing the AI to consider and evaluate potential moves based on their specific locations on the game board.
+ * It simplifies the process of passing around move coordinates within the AI's logic.
+ */
+
 package application;
 
 /**
